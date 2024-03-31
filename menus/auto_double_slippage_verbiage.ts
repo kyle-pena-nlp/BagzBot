bd112a3d042f6555c70216ed7f035cc0aa3cd3e6
Kyle Pena <kp1197@gmail.com>
2024-03-30 22:56:16 -0400

    punctuation

--- menus/auto_double_slippage_verbiage.ts
+++ menus/auto_double_slippage_verbiage.ts
@@ -3,5 +3,5 @@ export function addAutoDoubleSlippageVerbiage(lines : string[]) {
     lines.push(':bullet: Choose whether you would like to automatically double the slippage percent every time the auto-sell fails due to slippage tolerance being exceeded.');
     lines.push(':bullet: If you do not choose to auto-double and the price drops very rapidly, you may not get out fast enough.');
     lines.push(':bullet: But if you choose to auto-double, you may lose out on profits if the token recovers or does not drop as rapidly.');
-    lines.push(':bullet: Use your best judgment.');
+    lines.push(':bullet: Use your best judgment!');
 }
