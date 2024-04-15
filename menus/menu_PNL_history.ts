cc26ca6e297b1b199c02da3e4029afce51cfa04b
Kyle Pena <kp1197@gmail.com>
2024-04-15 18:13:40 -0400

    redid pnl history to total by token

--- menus/menu_PNL_history.ts
+++ menus/menu_PNL_history.ts
@@ -1,6 +1,8 @@
-import { DecimalizedAmount, asTokenPriceDelta } from "../decimalized/decimalized_amount";
+import { dAdd, dDiv, dMult } from "../decimalized";
+import { DecimalizedAmount, MATH_DECIMAL_PLACES, asPercentDeltaString, asTokenPriceDelta, dZero, fromNumber } from "../decimalized/decimalized_amount";
 import { Position } from "../positions";
 import { CallbackButton } from "../telegram";
+import { groupIntoMap } from "../util";
 import { CallbackData } from "./callback_data";
 import { logoHack } from "./logo_hack";
 import { Menu, MenuCapabilities } from "./menu";
@@ -14,17 +16,46 @@ export class MenuPNLHistory extends Menu<{ closedPositions : Position[], netPNL 
 
         const closedPositions = this.menuData.closedPositions;
         closedPositions.sort((a,b) => (a.txBuyAttemptTimeMS||0 - b.txBuyAttemptTimeMS||0))
-        for (const pos of closedPositions) {
+        
+        const groupedClosedPositions = groupIntoMap(closedPositions, pos => pos.token.address);
+        for (const [address,closedPositionsForToken] of groupedClosedPositions) {
+            this.addPNLSummaryForToken(lines, closedPositionsForToken);
+        }
+        
+        /*for (const pos of closedPositions) {
             this.addClosedPositionSummary(lines, pos);
         }
         if (this.menuData.closedPositions.length == 0) {
             lines.push("<blockquote>Your don't have any closed positions yet!  Your total earnings will show here when you do.</blockquote>");
         }
-        lines.push("");
+        lines.push("");*/
 
         this.addNetPNLSummary(lines);
         return lines.join("\r\n");
     }
+
+    addPNLSummaryForToken(lines : string[], closedPositionsForToken : Position[]) {
+        let netPNL = dZero();
+        let totalInvested = dZero();
+        let numPositions = 0;
+        for (const closedPosition of closedPositionsForToken) {
+            const pnl = closedPosition.netPNL;
+            if (pnl != null) {
+                netPNL = dAdd(netPNL, pnl);
+                totalInvested = dAdd(totalInvested, closedPosition.vsTokenAmt);
+                numPositions += 1;
+            }
+        }
+        const pos = closedPositionsForToken[0];
+        const pnlPercentDelta = dMult(fromNumber(100), dDiv(netPNL, totalInvested, MATH_DECIMAL_PLACES)||dZero());
+        lines.push(`<b><u>$${pos.token.symbol}:</u></b>`);
+        lines.push(`:bullet: ${numPositions} closed position` + ((numPositions > 1) ? 's' : '') );
+        lines.push(`:bullet: ${asTokenPriceDelta(netPNL)} SOL`);
+        lines.push(`:bullet: ${asPercentDeltaString(pnlPercentDelta)} PNL`);
+        lines.push("");
+        //lines.push(summaryLine);
+    }
+
     renderOptions(): CallbackButton[][] {
         const options = this.emptyMenu();
         this.insertButtonNextLine(options, `:refresh: Refresh`, new CallbackData(MenuCode.ViewPNLHistory));
@@ -32,7 +63,7 @@ export class MenuPNLHistory extends Menu<{ closedPositions : Position[], netPNL 
         return options;
     }
     private addHeader(lines : string[]) {
-        lines.push(`${logoHack()}<u><b>PNL History</b></u>`);        
+        lines.push(`${logoHack()}<u><b>PNL Summary</b></u>`);        
     }
     private addNetPNLSummary(lines : string[]) {
         lines.push(`<b>Total</b>: <code>${asTokenPriceDelta(this.menuData.netPNL)} SOL</code>`);
